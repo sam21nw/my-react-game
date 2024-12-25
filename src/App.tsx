9b68e510200c966a2527ae1017d0efd4bac62eca
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-12-25 10:11:02 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Square from "./ui/Square.tsx";
+
+function App() {
+
+  return (
+    <>
+        <Square/>
+    </>
+  )
+}
+
+export default App
---
+++ src/ui/Square.tsx
@@ -0,0 +1,8 @@
+﻿export default function Square() {
+    return (
+        <>
+            <button className="square">X</button>
+            <button className="square">X</button>
+        </>
+    )
+}
